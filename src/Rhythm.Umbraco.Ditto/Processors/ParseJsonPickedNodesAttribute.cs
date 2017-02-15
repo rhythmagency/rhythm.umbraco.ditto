cb8c3f37430234eed9bf1bc9d7671c17bf886435
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2017-02-15 12:20:08 -0800

    Null check fix and cleanup for ParseJsonPickedNodesAttribute.

--- src/Rhythm.Umbraco.Ditto/Processors/ParseJsonPickedNodesAttribute.cs
+++ src/Rhythm.Umbraco.Ditto/Processors/ParseJsonPickedNodesAttribute.cs
@@ -1,23 +1,54 @@
 ﻿namespace Rhythm.Umbraco.Ditto.Processors
 {
-    using System;
-    using System.Collections.Generic;
+
+    // Namespaces.
     using Newtonsoft.Json;
     using Our.Umbraco.Ditto;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
+    /// <summary>
+    /// Parses JSON containing picked content node ID's.
+    /// </summary>
     [AttributeUsage(validOn: AttributeTargets.Property, AllowMultiple = true)]
     public class ParseJsonPickedNodesAttribute : UmbracoPropertyAttribute
     {
+
+        #region Methods
+
+        /// <summary>
+        /// Returns a collection of node ID's (each being a string representation of an integer).
+        /// </summary>
+        /// <returns>
+        /// The collection of node ID's.
+        /// </returns>
         public override object ProcessValue()
         {
+
+            // Validate input.
+            if (Value == null)
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            // Deserialize JSON.
             var nodeIds = new List<string>();
-            if (this.Value != null) return nodeIds.ToArray();
-            var json = this.Value is string ? JsonConvert.DeserializeObject<dynamic>((string) this.Value) : this.Value;
+            var json = Value is string
+                ? JsonConvert.DeserializeObject<dynamic>(Value as string)
+                : Value;
+
+            // Convert to list of strings, and return.
             foreach (var nodeId in json)
             {
                 nodeIds.Add(nodeId.Value as string);
             }
-            return nodeIds.ToArray();
+            return nodeIds;
+
         }
+
+        #endregion
+
     }
+
 }
